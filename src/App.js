3ee5fc34761689f5a29a7ec58a61c3d2e0a43fac
Reshma Rajan K <reshmarajank199@gmail.com>
2021-12-07 19:50:10 +0400

    counter assigment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [maxvalue, setMaxvalue] = useState(1000) //set 1000 as maxmimum value as default
+  const [minvalue, setMinvalue] = useState(1) //set 1 as minimum value as default
+  const [count, setCount] = useState(minvalue)
+  //increment count by 1 in each "increment button" press up to maximum limit
+  const incrementHandler = () => {
+    if (count < maxvalue) {
+      setCount(count + 1)
+    }
+  }
+  //decrement count by 1 in each "decrement button" press up to mimimum limit
+  const decrementHandler = () => {
+    if (minvalue < count)
+      setCount(count - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <label>Enter minimum limit</label>
+      <input type="number" placeholder=" Enter minimum limit" name=" minimum" onChange={(e) => setMinvalue(e.target.value)} value={minvalue} />
+      <label>Enter maximum limit</label>
+      <input type="number" placeholder=" Enter maximum limit" name=" maximum" onChange={(e) => setMaxvalue(e.target.value)} value={maxvalue} />
+      <h1>{count}</h1>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
+    </div >
   );
 }
 
